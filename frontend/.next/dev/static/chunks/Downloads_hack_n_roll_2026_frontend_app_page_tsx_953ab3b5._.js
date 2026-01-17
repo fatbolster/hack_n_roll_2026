667af27170835b6e575c8ccd45b5f43fc13f5e1b
Chua Kai Jie, Emerson <emersonchua934@outlook.com>
2026-01-17 13:39:30 +0800

    Edit the structure

---
+++ frontend/.next/dev/static/chunks/Downloads_hack_n_roll_2026_frontend_app_page_tsx_953ab3b5._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/2bfcd_next_dist_62cb1bd9._.js"
+],
+    source: "dynamic"
+});
